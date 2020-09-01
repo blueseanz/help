cfa97e24d2c4c34900a40d36c565a9373d93c5c6
ming <ming.fengnz@gmail.com>
2020-09-02 10:38:18 +1200

    add basic geolocation

--- js/main.js
+++ js/main.js
@@ -73,8 +73,7 @@ window.onload = function() {
 		profileLink.addEventListener('click', getProfile, false);
 
 	})();
-		/* --------- login module end  -------------- */
-
+	/* --------- login module end  -------------- */
 
 	/* --------- make donation module start -------------- */
 	(function() {
@@ -86,12 +85,12 @@ window.onload = function() {
 		var donateLaterLink = document.getElementById('donate-later-link');
 		var checkedInd = 2;
 
-		function showDotationForm() {		
+		function showDotationForm() {
 			donationAddress.style.display = "none";
 			donateFormContainer.style.display = "block";
 		}
 		donateBotton.addEventListener('click', showDotationForm, false);
-		
+
 		//uncheck selected radio buttons if custom amount was choosen
 		function onCustomAmountFocus() {
 			for (var i = 0; i < donateForm.length; i++) {
@@ -107,7 +106,7 @@ window.onload = function() {
 			}
 		}
 		customAmount.addEventListener('focus', onCustomAmountFocus, false);
-		
+
 		function onCustomAmountBlur() {
 			var value = customAmount.value;
 
@@ -117,14 +116,74 @@ window.onload = function() {
 			}
 		}
 		customAmount.addEventListener('blur', onCustomAmountBlur, false);
-		
-		function donateLater(){
+
+		function donateLater() {
 			donationAddress.style.display = "block";
 			donateFormContainer.style.display = "none";
 		}
 		donateLaterLink.addEventListener('click', donateLater, false);
-		
+
 	})();
 	/* --------- make donation module end -------------- */
+
+	/* --------- basic geolocation module start -------------- */
+	(function() {
+	
+		var mapContainer = document.getElementById('map-container');
+		
+		// Success handler
+		function successGeoData(position) {
+			var successMessage = "We found your position!";
+			successMessage += '\n Latitude = ' + position.coords.latitude;
+			successMessage += '\n Longitude = ' + position.coords.longitude;
+			successMessage += '\n Accuracy = ' + position.coords.accuracy + ' meters';			
+			console.log(successMessage);
+			
+			var successMessageHTML = successMessage.replace(/\n/g, '<br />');
+			var currentContent = mapContainer.innerHTML;
+			mapContainer.innerHTML = currentContent + "<br />" + successMessageHTML;
+			
+		}
+
+        // error handler
+		function failGeoData(error) {
+			console.log('error code = ' + error.code);
+			
+			switch(error.code) {
+				case error.POSITION_UNAVALABLE:
+					errorMessage = "Can't get the location";
+					break;
+				case error.PERMISSION_DENIED:
+					errorMessage = "The user doesn't want to share location";
+					break;
+				case error.TIMEOUT:
+					errorMessage = "Timeout - Finding location takes too long";
+					break;
+				case error.UNKNOWN_ERROR:
+					errorMessage = "Unknown error: " + error.code;
+					break;
+			}
+			console.log(errorMessage);
+			mapContainer.innerHTML = errorMessage;	
+		}
+
+		if (navigator.geolocation) {
+			var startMessage = 'Browser supports geolocation API';
+			console.log(startMessage);
+			mapContainer.innerHTML = startMessage;
+			console.log('Checking your position...');
+			mapContainer.innerHTML = startMessage + '<br />Checking your position...';
+			
+			navigator.geolocation.getCurrentPosition(successGeoData, failGeoData, {
+				maximumAge : 60000,
+				enableHighAccuracy : true,
+				timeout : 5000
+			});
+		} else {
+			mapContainer.innerHTML ='Your browser does not support geolocation';
+		}
+		
+	})();
+	/* --------- basic geolocation module end -------------- */
 
 }
