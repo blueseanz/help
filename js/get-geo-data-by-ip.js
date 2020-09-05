30f6887bfe13ce29233a97cb4603b181abc7cec4
ming <ming.fengnz@gmail.com>
2020-09-05 21:37:38 +1200

    remove modernizr

--- js/get-geo-data-by-ip.js
+++
@@ -1,9 +0,0 @@
-function init(){
-
-	var locationMap = document.getElementById('location-map');
-	locationMap.innerHTML = "Your browser does not support HTML 5 geolocation API.";
-
-	// The code to get the location by IP from http://j.maxmind.com/app/geoip.js will go here
-	// then we'll pass the latitude and longitude values to the Google Map API for drawing the map.
-	
-}
